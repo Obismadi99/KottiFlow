6cf381ab93ae99a9db514d62465782c83c5b5a75
Alberich25 <matthieu.oberon@gmail.com>
2022-07-14 16:37:43 +0200

    add installation of RPI-RGB-Matri in setup

--- LED_Matrix/setup.sh
+++ LED_Matrix/setup.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-# Download configuration and python files
+# Install RGB-Matrices drivers
+echo "----------------------------------------------------------------------------"
+echo "---------------------- Install RGB-Matrices drivers ------------------------"
+echo "----------------------------------------------------------------------------"
 cd ~
-curl https://raw.githubusercontent.com/Obismadi99/KottiFlow/main/LED_Matrix/Arrows.py > ~/rpi-rgb-led-matrix/bindings/python/samples/Arrows.py
-curl https://raw.githubusercontent.com/Obismadi99/KottiFlow/main/LED_Matrix/ecosystem.config.js > ~/ecosystem.config.js
+curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/rgb-matrix.sh > rgb-matrix.sh
+sudo bash rgb-matrix.sh
 
 # Install npm and pm2
+echo "----------------------------------------------------------------------------"
+echo "-------------------------- Install npm and PM2 -----------------------------"
+echo "----------------------------------------------------------------------------"
 sudo apt install npm
 sudo npm install -g pm2@latest
 
 # Install numpy
+echo "----------------------------------------------------------------------------"
+echo "----------------------------- Install numpy --------------------------------"
+echo "----------------------------------------------------------------------------"
 pip3 install numpy
 
-# Setup automatic start for Arrowas
+# Download configuration and python files
+echo "----------------------------------------------------------------------------"
+echo "----------------- Download configuration and python files ------------------"
+echo "----------------------------------------------------------------------------"
+cd ~
+curl https://raw.githubusercontent.com/Obismadi99/KottiFlow/main/LED_Matrix/Arrows.py > ~/rpi-rgb-led-matrix/bindings/python/samples/Arrows.py
+curl https://raw.githubusercontent.com/Obismadi99/KottiFlow/main/LED_Matrix/ecosystem.config.js > ~/ecosystem.config.js
+
+# Setup automatic start for Arrows.py
+echo "----------------------------------------------------------------------------"
+echo "------------------- Setup automatic start for Arrows.py --------------------"
+echo "----------------------------------------------------------------------------"
 sudo pm2 start ecosystem.config.js
 sudo pm2 save
 sudo pm2 startup
