fa96ce451ea20cf8652f57bdeba2790dabb47e6a
Alberich25 <matthieu.oberon@gmail.com>
2022-07-14 16:17:36 +0200

    fix setup.sh

--- LED_Matrix/ecosystem.config.js
+++ LED_Matrix/ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	apps : [{
-	name :  "RadbahnAmpel",
+	name :  "Arrows",
 	script : "testArrows.py",
 	cwd : "/home/pi/rpi-rgb-led-matrix/bindings/python/samples",
 	args : "-t 0.01 --triangle-height-min=20 --triangle-width=32 --n-reshape-triangle=15 -c 3",
--- LED_Matrix/setup.sh
+++ LED_Matrix/setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Download configuration and python files
 cd ~
-curl https://github.com/Obismadi99/KottiFlow/blob/main/LED_Matrix/Arrows.py > ~/rpi-rgb-led-matrix/bindings/python/samples/Arrows.py
-curl https://github.com/Obismadi99/KottiFlow/blob/main/LED_Matrix/ecosystem.config.js > ~/ecosystem.config.js
+curl https://raw.githubusercontent.com/Obismadi99/KottiFlow/main/LED_Matrix/Arrows.py > ~/rpi-rgb-led-matrix/bindings/python/samples/Arrows.py
+curl https://raw.githubusercontent.com/Obismadi99/KottiFlow/main/LED_Matrix/ecosystem.config.js > ~/ecosystem.config.js
 
+# Install npm and pm2
 sudo apt install npm
-sudo npm install pm2@latest -g
+sudo npm install -g pm2@latest
 
+# Install numpy
+pip3 install numpy
+
+# Setup automatic start for Arrowas
 sudo pm2 start ecosystem.config.js
 sudo pm2 save
 sudo pm2 startup
